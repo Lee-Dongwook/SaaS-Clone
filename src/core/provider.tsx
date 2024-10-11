783694cafb7fef212046adc75ba0c7d5bbe83c1a
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-11 09:55:01 +0900

    feat : Tanstack Query Provider

--- src/core/provider.tsx
+++ src/core/provider.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "@/redux/store";
 
 const ClientProvider = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </Provider>
+  );
 };
 
 export default ClientProvider;
