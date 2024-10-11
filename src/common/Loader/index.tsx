d9d2c68112027c9fc37ca9dd4d0a466b6fa219b1
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-11 17:05:55 +0900

    hotfix

--- src/common/Loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Loader = () => {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="loader"></div>
-    </div>
-  );
-};
