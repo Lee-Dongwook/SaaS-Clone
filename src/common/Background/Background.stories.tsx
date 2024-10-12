47022fbaf54535ad9dd50cfdca5c3563fde02405
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-12 11:56:12 +0900

    feat : 공통 컴포넌트 작성

--- src/common/Background/Background.stories.tsx
+++ src/common/Background/Background.stories.tsx
@@ -1,12 +1,12 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react';
 
-import { Background } from '.'
+import { Background } from '.';
 
 const meta: Meta<typeof Background> = {
   title: 'Components/Background',
   component: Background,
   parameters: {
-    layout: 'fullscreen'
+    layout: 'fullscreen',
   },
   tags: ['autodocs'],
 } satisfies Meta<typeof Background>;
@@ -16,13 +16,13 @@ type Story = StoryObj<typeof meta>;
 
 export const DefaultBackgroundWithChildren = {
   args: {
-    children: <div>Children Node</div>
-  }
+    children: <div>Children Node</div>,
+  },
 } satisfies Story;
 
 export const RedBackgroundWithChildren = {
   args: {
     className: 'bg-red-500',
-    children: <div>Children Node</div>
-  }
+    children: <div>Children Node</div>,
+  },
 } satisfies Story;
